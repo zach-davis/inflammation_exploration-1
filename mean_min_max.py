496bed4cf28ce70f8e5856ed07b38f92536e4d0f
ryanshewcraft <ryan.shewcraft@nyu.edu>
2018-03-07 10:28:23 -0500

    Changed fig order

--- mean_min_max.py
+++ mean_min_max.py
@@ -7,9 +7,9 @@ data = numpy.loadtxt(fname='inflammation-01.csv', delimiter=',')
 #make three subplots (mean, max, min)
 fig = matplotlib.pyplot.figure(figsize=(10.0, 3.0))
 
-axes1 = fig.add_subplot(1, 3, 1)
-axes2 = fig.add_subplot(1, 3, 2)
-axes3 = fig.add_subplot(1, 3, 3)
+axes1 = fig.add_subplot(1, 3, 3)
+axes2 = fig.add_subplot(1, 3, 1)
+axes3 = fig.add_subplot(1, 3, 2)
 
 axes1.set_ylabel('average')
 axes1.plot(numpy.mean(data, axis=0))
